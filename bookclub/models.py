eb194937fb3056451a286da7e78a5c50e5bbe77b
CubeddL2 <skewbiscool@gmail.com>
2026-02-28 22:08:22 +0800

    Added book_list.html and book.html templates to show the list of books and its details

--- bookclub/models.py
+++ bookclub/models.py
@@ -9,16 +9,33 @@ class Genre(models.Model): #Should be sorted by name in ascending order
         return'{}'.format(self.name)
     
     def get_absolute_url(self):
-        return reverse('bookclub:bookclub', args=[str(self.name)])
+        return reverse('bookclub:book_list', args=[str(self.name)])
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'genre'
+        verbose_name_plural = 'genres'
+
 
 class Book(models.Model): #Should be sorted by the date it was published in descending order
     title = models.CharField(max_length=255)
     genre = models.ForeignKey(
         Genre,
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
         related_name="books"
     )
     author = models.CharField()
     pubYear = models.IntegerField()
     createdOn = models.DateTimeField() #only gets set when the model is created
     updatedOn = models.DateTimeField() #always updated on the last model update
+
+    def __str__(self):
+        return'{}'.format(self.title)
+    
+    def get_absolute_url(self):
+        return reverse('bookclub:book', args=[str(self.pk)])
+    
+    class Meta:
+        ordering = ['pubYear']
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
--- bookclub/urls.py
+++ bookclub/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name="index"),
-    #path('books/', BooksListView.as_view(), name='books'),
-    #path('book/<int:pk>', BooksDetailView.as_view(), name='book'),
+    path('books/', BookListView.as_view(), name='books'),
+    path('book/<int:pk>', BookDetailView.as_view(), name='book'),
 ]
 
 app_name = "bookclub"
