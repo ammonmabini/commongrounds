4e7c1e66df41bb6d3a2beeca7f7c9d4dd838ee7d
CubeddL2 <skewbiscool@gmail.com>
2026-02-28 20:46:31 +0800

    Added Genre and Book models in models.py

--- bookclub/models.py
+++ bookclub/models.py
@@ -7,12 +7,18 @@ class Genre(models.model): #Should be sorted by name in ascending order
 
     def __str__(self):
         return'{}'.format(self.name)
+    
     def get_absolute_url(self):
         return reverse('bookclub:bookclub', args=[str(self.name)])
 
 class Book(models.model): #Should be sorted by the date it was published in descending order
     title = models.CharField(max_length=255)
     author = models.CharField()
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name="books"
+    )
     pubYear = models.IntegerField()
     createdOn = models.DateTimeField() #only gets set when the model is created
     updatedOn = models.DateTimeField() #always updated on the last model update
